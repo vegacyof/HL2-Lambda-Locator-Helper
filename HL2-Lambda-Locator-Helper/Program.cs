1b3965ef0f510c8d6d24fe1ab4c89f89c75475e2
vegacyof <persqoff@gmail.com>
2026-06-10 18:03:12 +0400

    Init commit

---
+++ HL2-Lambda-Locator-Helper/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HL2LambdaLocatorHelper
+{
+    class Program
+    {
+        static void Main()
+        {
+            DisplayMenu();
+        }
+
+        static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("HL2 Lambda Locator Helper");
+            Console.WriteLine("\nEnter your hex string (For help, write \"?\")");
+            string input = Console.ReadLine();
+            InputHandler(input);
+        }
+        
+        static void InputHandler(string input)
+        {
+            var pattern = new Regex(@"^(0x)[0-9A-Fa-f]{16}$");
+            bool isHex = pattern.IsMatch(input);
+
+            if (isHex == true)
+            {
+                HexHandler(input);
+            }
+
+            else
+            {
+                switch (input)
+                {
+                    case "":
+                        Environment.Exit(0);
+                        break;
+
+                    case "?":
+                        DisplayHelp();
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("You did not enter a HEX string");
+                        Console.ReadKey();
+                        DisplayMenu();
+                        break;
+                }
+            }
+        }
+
+        static void DisplayHelp()
+        {
+            Console.Clear();
+            Console.WriteLine("Open the file gamestate.txt from the steamapps\\common\\Half-Life 2\\hl2_complete folder. \nFind the string \"id\" \"86\" in it. \nJust below, find \"data\" \"0x0000????????????\", Where is \"?\" - numbers and letters. \nCopy your value \"0x0000????????????\" without quotes. This will be your HEX string.\nPress any key to return");
+            Console.ReadKey();
+            DisplayMenu();
+        }
+
+        static void HexHandler(string hex)
+        {
+            long number = Convert.ToInt64(hex, 16);
+            string reservedBinary = new string(Convert.ToString(number, 2).Reverse().ToArray());
+            int i = 1;
+            bool allCollected = !reservedBinary.Contains('0');
+
+            if (allCollected == true)
+            {
+                Console.WriteLine("All collected!");
+            }
+            else
+            {
+                StringBuilder lambdas = new StringBuilder();
+                foreach (var bit in reservedBinary)
+                {
+                    if (bit == '0')
+                    {
+                        lambdas.AppendLine($"{i}: Missing");
+                    }
+
+                    i++;
+                }
+
+                Console.WriteLine(lambdas);
+            }
+
+            Console.ReadKey();
+            DisplayMenu();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# HL2-Lambda-Locator-Helper
+
+A simple C# console program that reads your HEX string and shows which stashes (lambdas) you missed in Half-Life 2. Helps you unlock the "Lambda Locator" achievement.
+
+## How to use
+
+1. [Download .NET 9.0+](https://dotnet.microsoft.com/en-us/download)
+2. Go to the RELEASE page and download the file `HL2LambdaLocatorHelper.exe`. Run it.
+3. Enter your hex string and get the result (For help, enter `?` in the program window)
